7f075546356e1c98e1419f437ca786f43d612189
Dhana Santini <dhana2000801@outlook.com>
2026-04-15 00:04:16 +0300

    fix backend error

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Weather API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://localhost:5174"],
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
